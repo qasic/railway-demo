de75198eea939f2a27648df7132cde5839775568
K Fung <github@qasic.com>
2023-06-18 14:23:00 -0700

    Disable ExpressJS caching

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const nocache = require('nocache');
+
 const app = express();
 const port = 3000;
+
+// disable ExpressJS caching
+// see https://stackoverflow.com/a/53240717
+app.set('etag', false);
+app.use(nocache());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
